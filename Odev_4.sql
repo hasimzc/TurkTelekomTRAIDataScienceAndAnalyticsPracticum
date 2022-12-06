e4845fb5e05a385dd65893ef4576cf937ad8c208
Haşim Zafer ÇİÇEK <89254814+hasimzc@users.noreply.github.com>
2022-12-06 15:52:15 +0300

    Update Odev_4.sql

--- Odev_4.sql
+++ Odev_4.sql
@@ -1,8 +1,8 @@
--- 1- SELECT country FROM country
--- WHERE country LIKE 'A%a';
--- 2-SELECT country FROM country
--- WHERE country LIKE '_____%n';
--- 3- SELECT title FROM film
--- WHERE title ILIKE '%t%t%t%t'
--- 4- SELECT * FROM film
--- WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+SELECT country FROM country
+WHERE country LIKE '_____%n';
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t'
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
